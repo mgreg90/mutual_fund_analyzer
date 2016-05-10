1adca29897ae1e716cc730e10d76c070b211e153
Mike Gregory <mgregory8219@gmail.com>
2016-05-10 10:35:13 -0400

    updated mf list model

--- app/models/mutual_fund_list.rb
+++ app/models/mutual_fund_list.rb
@@ -1,14 +1,14 @@
 class MutualFundList < ActiveRecord::Base
 
-  def get_raw_list
+  def get_raw_list_mstar_cats
     url = "http://news.morningstar.com/fund-category-returns/print.aspx"
     response = HTTParty.get url
     dom = Nokogiri::HTML(response.body)
   end
 
-  def push_raw_list_into_array
+  def push_raw_list_mstar_cats_into_array
     temp_arr = []
-    dom = get_raw_list
+    dom = get_raw_list_mstar_cats
     dom.css('td').each do |td|
       if td['colspan'] != '7'
         # puts td.text
@@ -19,10 +19,10 @@ class MutualFundList < ActiveRecord::Base
     temp_arr
   end
 
-  def save_array_of_returns_to_database
-    temp_arr = push_raw_list_into_array
-    temp_arr = temp_arr.each_slice(7)
-    p temp_arr
+  def save_array_of_mstar_cats_to_database
+    temp_arr = push_raw_list_mstar_cats_into_array
+    temp_arr = temp_arr.each_slice(7).to_a
+    # p temp_arr
   end
 
 end
